c61b7b9be14df545923125e4e03672b7ae80cd6f
tojoiv <tojoiv@gmail.com>
2016-01-12 12:39:58 +0100

    Ajout de la fonction getHeigth

--- tableModel.js
+++ tableModel.js
@@ -21,6 +21,7 @@ function tableModel (w, h) {
 
 tableModel.prototype = function(){
 	return {
-		getWidth : function() {return this.width}
-	}
+		getWidth : function() {return this.width;},
+		getHeigth : function() {return this.heigth;},
+	};
 }();
